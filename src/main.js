a5a66f6e2f22c323f0aec83d5d8f99bc3cc21aa2
カンフー豚 <46121989+Ashley-zm@users.noreply.github.com>
2022-03-22 16:59:16 +0800

    浏览器title设置

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+//icon css引用
 import '@/assets/icon/iconfont.css'
 // symbol 引用
-import '@/assets/iconfont/iconfont.js'
-import '@/assets/iconfont/iconfont.css'
+// import '@/assets/iconfont/iconfont.js'
+// import '@/assets/iconfont/iconfont.css'
 //设置全局icon样式
-import '@/assets/iconGloble.css'
-
+// import '@/assets/iconGloble.css'
+//svg
 import '@/assets/icons'
 
 Vue.config.productionTip = false
@@ -17,3 +18,12 @@ new Vue({
   // 注册路由：写法省略了k：v的方式，直接写k（router小写）
   router
 }).$mount('#app')
+//显示浏览器title
+router.beforeEach((to,from, next) => {
+  /* 路由发生变化修改页面title */
+  console.log(to.meta.title);
+  if (to.meta.title) {
+   document.title = to.meta.title
+  }
+  next();
+});
